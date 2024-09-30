9e030c3555d26f7525beb41e9dc0b973005434b7
Sobuj <srsobuj150921@gmail.com>
2024-09-30 09:12:20 +0600

    Custom User Model Registration with email and JWT. login,password change,password reset link with email and confirm password reset

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,34 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
 
-# @admin.register(User)   # eta likhle nicher register ti likha lagbe na 
+# Register your models here.
+# admin.site.register(User)
+
+# @admin.register(User)   # when we write this line then last line not required
 class AccountAdmin(UserAdmin):
-    
     # Model Field Names.....They will be displayed as columns in the admin list view.
-    """ list_display = ('email', 'first_name', 'last_name', 'username', 'updated_at', 'created_at', 'is_active')
-    list_display = ['email', 'first_name', 'last_name', 'username', 'updated_at', 'created_at', 'is_active'] """
-    
-    
-    # Callable Methods on the Model
-    list_display = ['email', 'first_name', 'last_name','full_name','username',]
-    
-    
-    # Properties on the Model
-    # You can also use properties, which are similar to methods but are accessed like attributes (without parentheses). Properties allow you to calculate or format data dynamically.
-    
-    
-    """ # Callable function to display a custom active status message
-    #Callable functions can be used in admin.py to add custom logic that doesn't necessarily need to reside in the model itself. These functions take an instance of the model as an argument and return a value to be displayed in the admin interface.
-    def custom_active_status(obj):
-        if obj.is_active:
-            return "Active User"
-        return "Inactive User"
-    custom_active_status.short_description = 'Active Status'
-    list_display = ['email', 'first_name', 'last_name','username','custom_active_status']
-    # here we can't use full_name . if we use show this error: TypeError: AccountAdmin.custom_active_status() takes 1 positional argument but 2 were given """
-    
+    list_display = ('id','email', 'first_name', 'last_name','is_active') # list_display can be list or tuple
+    # list_display = ('email', 'first_name', 'last_name', 'username', 'updated_at', 'created_at', 'is_active') # list_display can be list or tuple
+ 
+    # def custom_active_status(self,obj):
+    #     if obj.is_active:
+    #         return "Active User"
+    #     return "Inactive User"
+    # custom_active_status.short_description = 'Active Status'
+    # list_display = ['email', 'first_name', 'last_name','username','full_name','custom_active_status']
     
     
     # fieldsets = [
--- accounts/models.py
+++ accounts/models.py
@@ -6,36 +6,34 @@ from django.utils import timezone
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self,email, password=None,**extra_fields):
         # create and return a regular user(or save a user) with an email and password 
         if not email:
             raise ValueError('Users must have an email address')
-
-        if not username:
-            raise ValueError('Users must have an username')
-
+        email = self.normalize_email(email)
         user = self.model(
-            email = self.normalize_email(email), # Normalize the email to a consistent format (lowercase)
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            # email = self.normalize_email(email), # Normalize the email to a consistent format (lowercase)
+            email=email,
+            **extra_fields
         ) # create a new user instance ...the user model is created using self.model which refers to custom user model 
 
         user.set_password(password) # set the password for the user (hash version) ..set_password method is used to hash the password before storing it 
         user.save(using=self._db) # save the user to the database 
         return user
 
-    def create_superuser(self, first_name, last_name, email, username, password=None):
-        # Same as create_user(), but also includes extra_fields which specify additional fields like is_staff and is_superuser.
-        # create and return (save) a super user with an email and password 
+    def create_superuser(self, email,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        # Ensure is_superuser and is_staff are True for superuse
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+        
+        # Create a new superuser
         user = self.create_user(
-            # email=email,
-            email, # we can use above line also
-            # email = self.normalize_email(email),
-            username = username,
-            password = password,
-            first_name = first_name,
-            last_name = last_name,
+            email,password,**extra_fields
         )
         user.is_admin = True
         user.is_active = True
@@ -45,45 +43,33 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     #TODO this should be rename to USER 
     email           = models.EmailField(max_length=100, unique=True)
     first_name      = models.CharField(max_length=50,blank=True)
     last_name       = models.CharField(max_length=50,blank=True)
-    username        = models.CharField(max_length=50)
-    phone_number    = models.CharField(max_length=50)
-
     # required
     created_at     = models.DateTimeField(auto_now_add=True)
     # created_at     = models.DateTimeField(default=timezone.now())
     updated_at      = models.DateTimeField(auto_now=True)
     is_admin        = models.BooleanField(default=False)
     is_staff        = models.BooleanField(default=False) #Determines if the user can access the admin interface.
-    is_active       = models.BooleanField(default=False) #Determines if the user account is active.
+    is_active       = models.BooleanField(default=True) #Determines if the user account is active.
+    # is_active       = models.BooleanField(default=False) #Determines if the user account is active.
     is_superadmin   = models.BooleanField(default=False)
 
+    # Specify the custom user manager
+    objects = MyAccountManager() # objects = MyAccountManager(): This sets MyAccountManager as the manager for this model. This means all interactions with the User model (such as creating users or querying users) will be handled by MyAccountManager.
+    # username=None
     USERNAME_FIELD = 'email' # This specifies the field to be used for login... use email to login instead of username 
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name'] # This is a list of fields that required when creating a superuser (besides the username_field) list any required fields besides 'email
-
-    objects = MyAccountManager() # objects = CustomUserManager(): This sets CustomUserManager as the manager for this model. This means all interactions with the CustomUser model (such as creating users or querying users) will be handled by CustomUserManager.
-        
-        
-    #Callable Methods on the Model
-    """ def full_name(self): 
-        return f'{self.first_name} {self.last_name}' """
+    # REQUIRED_FIELDS = ['username', 'first_name', 'last_name'] # This is a list of fields that required when creating a superuser (besides the username_field) list any required fields besides 'email
+    REQUIRED_FIELDS = [] # This is a list of fields that required when creating a superuser (besides the username_field) list any required fields besides 'email
     
+    def full_name(self): 
+        return f'{self.first_name} {self.last_name}'
     
-    @property
-    def full_name(self):
-        #Here, full_name is a property that dynamically combines the first_name and last_name
-        return f"{self.first_name} {self.last_name}".strip()
-        #The strip() ensures that it returns a clean string even if one of the fields is blank.
-    
-    
-    # def __str__(self):
-    #     return self.email     # This method defines how the object should be represented as a string. In this case, the user’s email is returned when the object is printed.
+    def __str__(self):
+        return str(self.id)+"-"+ self.email     # This method defines how the object should be represented as a string. In this case, the user’s email is returned when the object is printed.
     
     def get_email(self):
         return self.email
